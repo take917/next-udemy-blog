785587a7ad57834a140c37cd3896560be18e2afb
zero <1tktk2tk4@gmail.com>
2025-05-31 12:11:38 +0900

    add searchBox

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,32 @@
-export default function PostPage() {
-  return <div></div>;
+import PostCard from "@/components/post/PostCard";
+import { getPosts, searchPosts } from "@/lib/post";
+import { Post } from "@/types/post";
+
+type SearchParams = {
+  search?: string;
+};
+
+export default async function PostPage({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
+  const resolveSearchParams = await searchParams;
+  const query = resolveSearchParams.search || "";
+
+  const posts = query
+    ? ((await searchPosts(query)) as Post[])
+    : ((await getPosts()) as Post[]);
+  // const posts = (await getPosts()) as Post[];
+  return (
+    <>
+      <div className="container me-auto px-4 py-8">
+        <div className="grid grid-cols-1 md:grid-clos-2 lg:grid-cols-3 gap-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/layouts/PublicHeader.tsx
+++ src/components/layouts/PublicHeader.tsx
@@ -8,29 +8,29 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import SearchBox from "../post/SearchBox";
 
 export default function PublicHeader() {
   return (
     <div>
       <header className="border-b bg-blue-200">
         <div className="container mx-auto px-4 py-4 flex items-center justify-between">
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <Link href="/" passHref>
-                  <NavigationMenuLink className="font-bold text-xl">
-                    Blog
+          <nav>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuLink asChild>
+                    <Link href="/" className="font-bold text-xl">
+                      Blog
+                    </Link>
                   </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </nav>
 
           <div className="flex items-center gap-4">
-            <Input
-              placeholder="記事を検索..."
-              className="w-[200px] lg:w-[300px] bg-white"
-            />
+            <SearchBox />
             <Button variant="outline" asChild>
               <Link href="/login">ログイン</Link>
             </Button>

a9034fd53b0df1b964d507d391c0063de31e4964
zero <1tktk2tk4@gmail.com>
2025-06-03 19:25:04 +0900

    auth

---
+++ src/app/(private)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default async function DachBoardPage() {
+  return <div>dashboard</div>;
+}
---
+++ src/components/layouts/Setting.tsx
@@ -0,0 +1,34 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { Session } from "next-auth";
+
+export default function Setting({ session }: { session: Session }) {
+  const handleLogout = async () => {
+    "use server";
+    await signOut();
+  };
+
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="font-medium">
+            {session.user?.name}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem onClick={handleLogout} className="cursor-pointer">
+            ログアウト
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
